582111593102db4f351e78017871055bdd9ba403
Martin Pham <martin.pham.84@gmail.com>
2012-11-07 20:53:40 +1100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lesson8::Application.config.secret_token = 'fee70592eb80f1d4d7434334073eaec2301ed76e926ef97d9fbefe0da1fc48984268ede42c61adc7cec90ffc9d135b96d37565384a24018aef6e3beec8b81cfe'
